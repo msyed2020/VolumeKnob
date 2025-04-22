5fdf9704e3aa6b51bb76cd7d51784779ec988620
msyed2020 <mikailsyed@yahoo.com>
2025-04-22 14:16:15 -0400

    Add project files.

---
+++ VolumeKnob/source/cids.h
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2025 My Plug-in Company.
+//------------------------------------------------------------------------
+
+#pragma once
+
+#include "pluginterfaces/base/funknown.h"
+#include "pluginterfaces/vst/vsttypes.h"
+
+namespace MyCompanyName {
+//------------------------------------------------------------------------
+static const Steinberg::FUID kVolumeKnobProcessorUID (0x9E237997, 0x9FCE5739, 0x803E1F60, 0x069FCF96);
+static const Steinberg::FUID kVolumeKnobControllerUID (0x30E1A211, 0x70B150DD, 0x82A06BBC, 0x0CEF0EAF);
+
+#define VolumeKnobVST3Category "Fx"
+
+//------------------------------------------------------------------------
+} // namespace MyCompanyName
+
+enum {
+    kGainId = 1000
+};
---
+++ VolumeKnob/source/controller.cpp
@@ -0,0 +1,91 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2025 My Plug-in Company.
+//------------------------------------------------------------------------
+
+#include "controller.h"
+#include "cids.h"
+#include "vstgui/plugin-bindings/vst3editor.h"
+
+using namespace Steinberg;
+
+namespace MyCompanyName {
+
+//------------------------------------------------------------------------
+// VolumeKnobController Implementation
+//------------------------------------------------------------------------
+tresult PLUGIN_API VolumeKnobController::initialize (FUnknown* context)
+{
+	// Here the Plug-in will be instantiated
+
+	parameters.addParameter(
+		STR16("Gain"),      // Display name
+		nullptr,            // Unit (e.g., "dB", can be nullptr)
+		0,                  // Step count (0 = continuous)
+		1.0,                // Default value (1.0 = full volume)
+		Vst::ParameterInfo::kCanAutomate,
+		kGainId             // Unique parameter ID
+	);
+
+	//---do not forget to call parent ------
+	tresult result = EditControllerEx1::initialize (context);
+	if (result != kResultOk)
+	{
+		return result;
+	}
+
+	// Here you could register some parameters
+
+	return result;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API VolumeKnobController::terminate ()
+{
+	// Here the Plug-in will be de-instantiated, last possibility to remove some memory!
+
+	//---do not forget to call parent ------
+	return EditControllerEx1::terminate ();
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API VolumeKnobController::setComponentState (IBStream* state)
+{
+	// Here you get the state of the component (Processor part)
+	if (!state)
+		return kResultFalse;
+
+	return kResultOk;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API VolumeKnobController::setState (IBStream* state)
+{
+	// Here you get the state of the controller
+
+	return kResultTrue;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API VolumeKnobController::getState (IBStream* state)
+{
+	// Here you are asked to deliver the state of the controller (if needed)
+	// Note: the real state of your plug-in is saved in the processor
+
+	return kResultTrue;
+}
+
+//------------------------------------------------------------------------
+IPlugView* PLUGIN_API VolumeKnobController::createView (FIDString name)
+{
+	// Here the Host wants to open your editor (if you have one)
+	if (FIDStringsEqual (name, Vst::ViewType::kEditor))
+	{
+		// create your editor here and return a IPlugView ptr of it
+		auto* view = new VSTGUI::VST3Editor (this, "view", "editor.uidesc");
+		return view;
+	}
+	return nullptr;
+}
+
+//------------------------------------------------------------------------
+} // namespace MyCompanyName
